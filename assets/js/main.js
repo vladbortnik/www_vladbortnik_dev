6b496efea3860f8d22ba3f98aeabf4f19e983ee6
Vlad Bortnik <31524383+vladbortnik@users.noreply.github.com>
2025-04-12 04:19:19 -0400

    fix: carousel visibility

--- assets/js/main.js
+++ assets/js/main.js
@@ -234,19 +234,19 @@
   /**
    * Portfolio details slider
    */
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
+  // new Swiper('.portfolio-details-slider', {
+  //   speed: 400,
+  //   loop: true,
+  //   autoplay: {
+  //     delay: 5000,
+  //     disableOnInteraction: false
+  //   },
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //     type: 'bullets',
+  //     clickable: true
+  //   }
+  // });
 
   /**
    * Initiate Pure Counter 
